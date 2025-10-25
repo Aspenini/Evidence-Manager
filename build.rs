4ab96f12a12a308e0cf7845d92a47bea870c3ac8
Aspen <bobfeltner123@gmail.com>
2025-10-25 18:05:40 -0400

    add .desktop file for Linux builds

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 fn main() {
-    // Only compile the Windows resource on Windows
     #[cfg(windows)]
     {
         let mut res = winres::WindowsResource::new();
         res.set_icon("icons/icon.ico");
-        res.compile().unwrap();
+        res.compile().expect("Failed to compile Windows resources");
     }
-}
 
+    #[cfg(not(windows))]
+    {}
+}
